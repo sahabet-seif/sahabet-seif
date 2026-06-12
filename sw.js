77dfaa424ca727bd0dcc2650f97585f9926e7872
sahabet-seif <mahmoud.gamal11223389@gmail.com>
2026-06-13 00:52:15 +0300

    Updates

--- sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', (event) => {
-    console.log('تم تثبيت التطبيق بنجاح');
-});
-
-self.addEventListener('fetch', (event) => {
-    // الكود ده بيخلي التطبيق يستجيب للطلبات عشان يظهر كبرنامج
-});
